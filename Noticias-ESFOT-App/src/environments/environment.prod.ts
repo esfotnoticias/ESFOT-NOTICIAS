9cc017f54d0c3f8dce2a66bd23ade74fd2f82634
JonathanJMaiza <jonathanjavimaiza@gmail.com>
2021-01-03 18:31:39 -0500

    v1

---
+++ Noticias-ESFOT-App/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'sus_credenciales',
+    authDomain: 'sus_credenciales',
+    databaseURL: 'sus_credenciales',
+    projectId: 'sus_credenciales',
+    storageBucket: 'sus_credenciales',
+    messagingSenderId: 'sus_credenciales',
+    appId: 'sus_credenciales',
+    measurementId: 'sus_credenciales'
+  }
+};
